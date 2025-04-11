41d4e4a7bbcdcb8851c4659fbb15bca86acabb12
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-04-10 22:59:00 -0700

    Clean up old files

--- for_future_version/test-evi.R
+++ for_future_version/test-evi.R
@@ -6,25 +6,6 @@ e <- dst_gpd(0, 1, 0)
 f <- dst_norm(0, 1)
 g <- dst_empirical(-10:10)
 
-test_that("graft EVI works", {
-  expect_identical(
-    evi(graft_right(a, b, sep_y = 0.5)),
-    1
-  )
-  expect_identical(
-    evi(graft_right(a, c, sep_y = 0.5)),
-    -2
-  )
-  expect_identical(
-    evi(graft_right(b, f, sep_y = 0.5)),
-    0
-  )
-})
-
-check_mix <- function(..., evi) {
-  n <- length(list(...))
-  expect_identical(evi(mix(...)), evi)
-}
 
 test_that("mixture EVI works", {
   check_mix(a, b, evi = 1)
