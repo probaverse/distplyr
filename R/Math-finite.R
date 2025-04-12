7094c0ddfaaa540563fa008075ef74ee8263da91
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-04-12 11:42:58 -0700

    Clean up more unnecessary files or code for future

--- R/Math-finite.R
+++
@@ -1,6 +0,0 @@
-#' @export
-Math.finite <- function(x, ...) {
-	f <- .Generic[[1]]
-	call <- rlang::call2(f, rlang::expr(location))
-	mutate_finite(x, !!call)
-}
--- R/verb-invert.R
+++ R/verb-invert.R
@@ -31,3 +31,39 @@ invert <- function(distribution) {
   	dist, variable = distionary::variable(distribution), class = "inverse"
   )
 }
+
+#' @export
+eval_cdf.inverse <- function(distribution, at) {
+  dist <- distribution$distribution
+  distionary::eval_cdf(dist, at = 0) -
+    distionary::eval_cdf(dist, at = 1 / at) +
+    distionary::eval_pmf(dist, at = 1 / at, strict = FALSE) +
+    as.numeric(at >= 0)
+}
+
+#' @export
+eval_density.inverse <- function(distribution, at, strict) {
+  distionary::eval_density(
+    distribution$distribution, at = 1 / at, strict = strict
+  ) / at^2
+}
+
+#' @export
+eval_pmf.inverse <- function(distribution, at, strict) {
+  distionary::eval_pmf(
+    distribution$distribution, at = 1 / at, strict = strict
+  )
+}
+
+#' @export
+eval_quantile.inverse <- function(distribution, at) {
+  quantile_0 <- distionary::eval_quantile(distribution$distribution, at = 0)
+  1 / distionary::eval_quantile(
+    distribution, at = quantile_0 + (at > quantile_0) - at
+  )
+}
+
+#' @export
+realise.inverse <- function(distribution, n) {
+  1 / distionary::realise(distribution$distribution, n = n)
+}
