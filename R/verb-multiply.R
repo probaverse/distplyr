8daa587661775f427676d68a38c14655a570a197
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-04-12 16:57:59 -0700

    Refactor multiply

--- R/verb-multiply.R
+++ R/verb-multiply.R
@@ -9,15 +9,141 @@ multiply <- function(distribution, constant) {
     return(distribution)
   } else if (is.infinite(constant)) {
     stop("Cannot multiply a distribution by infinity.")
-  } else {
-    dist <- list(
-      components = list(
-        distribution = distribution,
-        scale = constant
-      )
-    )
-    distionary::new_distribution(
-    	dist, variable = distionary::variable(distribution), class = "scale"
-    )
   }
+  d <- distionary::distribution(
+    cdf = function(x) {
+      distionary::eval_cdf(distribution, at = x / constant)
+    },
+    survival = function(x) {
+      distionary::eval_survival(distribution, at = x / constant)
+    }
+    quantile = function(p) {
+      distionary::eval_quantile(distribution, at = p) * constant
+    },
+    pmf = function(x) {
+      distionary::eval_pmf(distribution, at = x / constant)
+    },
+    density = function(x) {
+      distionary::eval_density(distribution, at = x / constant) / constant
+    },
+    realize = function(n) {
+      distionary::realise(distribution, n = n) * constant
+    },
+    .vtype = distionary::vtype(distribution),
+    .name = "Scaled",
+    .parameters = list(
+      distribution = distribution,
+      constant = constant
+    )
+  )
+  if (distionary:::is_intrinsic(distribution, "mean")) {
+    d[["mean"]] <- mean(distribution) * constant
+  }
+  if (distionary:::is_intrinsic(distribution, "median")) {
+    d[["median"]] <- median(distribution) * constant
+  }
+  if (distionary:::is_intrinsic(distribution, "stdev")) {
+    d[["stdev"]] <- distionary::stdev(distribution) * constant
+  }
+  if (distionary:::is_intrinsic(distribution, "variance")) {
+    d[["variance"]] <- distionary::variance(distribution) * constant^2
+  }
+  if (distionary:::is_intrinsic(distribution, "skewness")) {
+    d[["skewness"]] <- distionary::skewness(distribution)
+  }
+  if (distionary:::is_intrinsic(distribution, "kurtosis_exc")) {
+    d[["kurtosis_exc"]] <- distionary::kurtosis_exc(distribution)
+  }
+  if (distionary:::is_intrinsic(distribution, "kurtosis_exc")) {
+    d[["kurtosis"]] <- distionary::kurtosis(distribution)
+  }
+  if (distionary:::is_intrinsic(distribution, "range")) {
+    d[["range"]] <- range(distribution) * constant
+  }
+  distionary::new_distribution(d, class = "scaled")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
