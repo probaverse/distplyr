eb499f25a9b8b88eb889e37799d85f734af8bb5f
Vincenzo Coia <vincenzo.coia@gmail.com>
2025-10-28 16:14:15 +0100

    Fix issue with negated distribution's quantiles at flat parts of the CDF.

--- R/verb-flip.R
+++ R/verb-flip.R
@@ -12,33 +12,40 @@ flip <- function(distribution) {
       mean = -mean(distribution),
       sd = stdev(distribution)
     ))
-  } else if (nm == "Uniform") {
+  }
+  if (nm == "Uniform") {
     p <- distionary::parameters(distribution)
     return(distionary::dst_unif(
       min = -p[["max"]],
       max = -p[["min"]]
     ))
-  } else if (nm == "Cauchy") {
+  }
+  if (nm == "Cauchy") {
     p <- distionary::parameters(distribution)
     return(distionary::dst_cauchy(
       location = -p[["location"]],
       scale = p[["scale"]]
     ))
-  } else if (nm == "Student t") {
+  }
+  if (nm == "Student t") {
     return(distribution)
-  } else if (nm == "Finite") {
+  }
+  if (nm == "Finite") {
     p <- distionary::parameters(distribution)
     return(distionary::dst_empirical(
       -p[["outcomes"]], weights = p[["probs"]]
     ))
-  } else if (nm == "Degenerate") {
+  }
+  if (nm == "Degenerate") {
     p <- distionary::parameters(distribution)
     return(distionary::dst_degenerate(-p[["location"]]))
-  } else if (nm == "Negated") {
+  }
+  if (nm == "Negated") {
     p <- distionary::parameters(distribution)
     return(p[["distribution"]])
   }
   ## END special simplifications -----------------------------------------------
+  ## (Except for quantile function)
   d <- distionary::distribution(
     cdf = function(x) {
       distionary::eval_pmf(distribution, at = -x) +
@@ -53,9 +60,6 @@ flip <- function(distribution) {
     },
     density = function(x) {
       distionary::eval_density(distribution, at = -x)
-    },
-    quantile = function(x) {
-      -distionary::eval_quantile(distribution, at = 1 - x)
     },
     realize = function(n) {
       -distionary::realize(distribution, n = n)
@@ -90,6 +94,24 @@ flip <- function(distribution) {
   }
   if (distionary:::is_intrinsic(distribution, "kurtosis")) {
     d[["kurtosis"]] <- distionary::kurtosis(distribution)
+  }
+  ## Quantile function simplifications:
+  ## Problem: eval_quantile(-dst_bern(0.5), at = 0.5) Should be -1, but will be
+  ## 0 if calculated as \(p) -eval_quantile(distribution, at = 1 - p)
+  ## because what we should actually be calculating is the right inverse of
+  ## `distribution`'s cdf.
+  ## For distributions with increasing CDF (no flat parts), this simplification
+  ## is OK. Otherwise (e.g., -mix(dst_unif(0, 1), dst_unif(2, 3))),
+  ## have to wait until right inverse functionality is implemented in distionary
+  has_inc_cdf <- c(
+    "Weibull", "Log Pearson Type III", "Pearson Type III",
+    "Generalized Extreme Value", "Generalized Pareto", "Beta", "Gamma",
+    "Exponential", "Log Normal", "F", "Chi-Squared"
+  )
+  if (nm %in% has_inc_cdf) {
+    d[["quantile"]] <- function(x) {
+      -distionary::eval_quantile(distribution, at = 1 - x)
+    }
   }
   distionary:::new_distribution(d, class = "negated")
 }
